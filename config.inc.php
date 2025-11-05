a705e6476279bd6485616c53b2ca710adcc9701e
Vitória Dantas <evihtoria@gmail.com>
2025-11-05 17:39:30 -0300

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
-$conexao = mysqli_connect("localhost", "root", "", "projeto1", 3310);
+$conexao = mysqli_connect("localhost","root","");
 
 if (!$conexao) {
     die(" Falha na conexão com o banco de dados: " . mysqli_connect_error());
